075c666bad82999962e3079ee683292cb6f35976
Nikolas Whiteside <nrw6218@rit.edu>
2018-09-17 21:20:58 -0400

    Fixes linting errors.

--- src/responses.js
+++ src/responses.js
@@ -1,10 +1,10 @@
-//function to send response
+// function to send response
 const respond = (request, response, content, status, type) => {
-  //set status code (200 success) and content type
+  // set status code (200 success) and content type
   response.writeHead(status, { 'Content-Type': type });
-  //write the content string or buffer to response
+  // write the content string or buffer to response
   response.write(content);
-  //send the response to the client
+  // send the response to the client
   response.end();
 };
 
@@ -13,19 +13,19 @@ const success = (request, response, acceptedType) => {
   if (acceptedType === 'text/xml') {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>This is a successful response.</message>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} </response>`;
 
-	  // return response passing out string and content type
+    // return response passing out string and content type
     return respond(request, response, responseXML, 200, 'text/xml');
   }
 
-  //create success message for response
+  // create success message for response
   const responseJSON = {
     message: 'This is a successful response.',
   };
 
   const responseString = JSON.stringify(responseJSON);
-  //return response passing json and content type
+  // return response passing json and content type
   return respond(request, response, responseString, 200, 'application/json');
 };
 
@@ -34,7 +34,7 @@ const badRequest = (request, response, acceptedType, queryParams) => {
   let responseMessage = 'Missing valid query parameter set to true.';
   let responseCode = 400;
 
-  if (queryParams.valid && queryParams.valid == 'true') {
+  if (queryParams.valid && queryParams.valid === 'true') {
     responseMessage = 'This request has the required parameters.';
     responseCode = 200;
   }
@@ -43,22 +43,22 @@ const badRequest = (request, response, acceptedType, queryParams) => {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>${responseMessage}</message>`;
     if (queryParams.valid !== 'true') responseXML = `${responseXML} <id>badRequest</id>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} </response>`;
 
-	  // return response passing out string and content type
+    // return response passing out string and content type
     return respond(request, response, responseXML, responseCode, 'text/xml');
   }
 
-  //create success message for response
-  let responseJSON = {
+  // create success message for response
+  const responseJSON = {
     message: responseMessage,
   };
 
   if (queryParams.valid !== 'true') responseJSON.id = 'badRequest';
 
   const responseString = JSON.stringify(responseJSON);
-  
-  //return response passing json and content type
+
+  // return response passing json and content type
   return respond(request, response, responseString, responseCode, 'application/json');
 };
 
@@ -67,7 +67,7 @@ const unauthorized = (request, response, acceptedType, queryParams) => {
   let responseMessage = 'Missing loggedIn query parameter set to yes.';
   let responseCode = 401;
 
-  if (queryParams.loggedIn && queryParams.loggedIn == 'yes') {
+  if (queryParams.loggedIn && queryParams.loggedIn === 'yes') {
     responseMessage = 'You have successfully viewed the content.';
     responseCode = 200;
   }
@@ -76,22 +76,22 @@ const unauthorized = (request, response, acceptedType, queryParams) => {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>${responseMessage}</message>`;
     if (queryParams.loggedIn !== 'yes') responseXML = `${responseXML} <id>unauthorized</id>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} </response>`;
 
-	  // return response passing out string and content type
+    // return response passing out string and content type
     return respond(request, response, responseXML, responseCode, 'text/xml');
   }
 
-  //create success message for response
-  let responseJSON = {
+  // create success message for response
+  const responseJSON = {
     message: responseMessage,
   };
 
   if (queryParams.loggedIn !== 'yes') responseJSON.id = 'unauthorized';
 
   const responseString = JSON.stringify(responseJSON);
-  
-  //return response passing json and content type
+
+  // return response passing json and content type
   return respond(request, response, responseString, responseCode, 'application/json');
 };
 
@@ -101,21 +101,21 @@ const forbidden = (request, response, acceptedType) => {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>You do not have access to this content.</message>`;
     responseXML = `${responseXML} <id>forbidden</id>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} </response>`;
 
-	  // return response passing out string and content type
+    // return response passing out string and content type
     return respond(request, response, responseXML, 403, 'text/xml');
   }
 
-  //create success message for response
+  // create success message for response
   const responseJSON = {
     message: 'You do not have access to this content.',
     id: 'forbidden',
   };
 
   const responseString = JSON.stringify(responseJSON);
-  
-  //return response passing json and content type
+
+  // return response passing json and content type
   return respond(request, response, responseString, 403, 'application/json');
 };
 
@@ -125,21 +125,21 @@ const internal = (request, response, acceptedType) => {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>Internal Server Error. Something went wrong.</message>`;
     responseXML = `${responseXML} <id>internal</id>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} </response>`;
 
-	  // return response passing out string and content type
+    // return response passing out string and content type
     return respond(request, response, responseXML, 500, 'text/xml');
   }
 
-  //create success message for response
+  // create success message for response
   const responseJSON = {
     message: 'Internal Server Error. Something went wrong.',
     id: 'internal',
   };
 
   const responseString = JSON.stringify(responseJSON);
-  
-  //return response passing json and content type
+
+  // return response passing json and content type
   return respond(request, response, responseString, 500, 'application/json');
 };
 
@@ -149,21 +149,21 @@ const notImplemented = (request, response, acceptedType) => {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>A get request for this page has not been implemented yet. Check again later for updated content.</message>`;
     responseXML = `${responseXML} <id>notImplemented</id>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} </response>`;
 
-	  // return response passing out string and content type
+    // return response passing out string and content type
     return respond(request, response, responseXML, 501, 'text/xml');
   }
 
-  //create success message for response
+  // create success message for response
   const responseJSON = {
     message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
     id: 'notImplemented',
   };
 
   const responseString = JSON.stringify(responseJSON);
-  
-  //return response passing json and content type
+
+  // return response passing json and content type
   return respond(request, response, responseString, 501, 'application/json');
 };
 
@@ -173,25 +173,25 @@ const notFound = (request, response, acceptedType) => {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>The page you are looking for was not found.</message>`;
     responseXML = `${responseXML} <id>notFound</id>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} </response>`;
 
-	  // return response passing out string and content type
+    // return response passing out string and content type
     return respond(request, response, responseXML, 404, 'text/xml');
   }
 
-  //create error message for response
+  // create error message for response
   const responseJSON = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
   };
 
   const responseString = JSON.stringify(responseJSON);
-  
-  //return response passing json and content type
+
+  // return response passing json and content type
   return respond(request, response, responseString, 404, 'application/json');
 };
 
-//set public modules
+// set public modules
 module.exports = {
   success,
   badRequest,
--- src/server.js
+++ src/server.js
@@ -1,31 +1,31 @@
-const http = require('http'); //http module
-const url = require('url'); //url module
+const http = require('http'); // http module
+const url = require('url'); // url module
 const queryString = require('query-string');
-const htmlHandler = require('./htmlResponses.js'); 
-const jsonHandler = require('./responses.js'); 
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//function to handle requests
+// function to handle requests
 const onRequest = (request, response) => {
-  //parse url into individual parts
-  //returns an object of url parts by name
+  // parse url into individual parts
+  // returns an object of url parts by name
   const parsedUrl = url.parse(request.url);
 
-  //grab any query parameters
+  // grab any query parameters
   const params = queryString.parse(parsedUrl.query);
 
-  //grab the 'accept' headers (comma delimited) and split them into an array
+  // grab the 'accept' headers (comma delimited) and split them into an array
   const acceptedTypes = request.headers.accept.split(',');
 
-  //check the request method (get, head, post, etc)
+  // check the request method (get, head, post, etc)
   switch (request.method) {
     case 'GET':
       if (parsedUrl.pathname === '/') {
-        //if homepage, send index
+        // if homepage, send index
         htmlHandler.getIndex(request, response);
       } else if (parsedUrl.pathname === '/style.css') {
-        //if stylesheet, send stylesheet
+        // if stylesheet, send stylesheet
         htmlHandler.getCSS(request, response);
       } else if (parsedUrl.pathname === '/success') {
         jsonHandler.success(request, response, acceptedTypes[0]);
@@ -40,17 +40,17 @@ const onRequest = (request, response) => {
       } else if (parsedUrl.pathname === '/notImplemented') {
         jsonHandler.notImplemented(request, response, acceptedTypes[0]);
       } else {
-        //if not found, send 404 message
+        // if not found, send 404 message
         jsonHandler.notFound(request, response, acceptedTypes[0]);
       }
       break;
     default:
-      //send 404 in any other case
+      // send 404 in any other case
       jsonHandler.notFound(request, response, acceptedTypes[0]);
   }
 };
 
-//start server
+// start server
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
